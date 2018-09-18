9fe926cee2c22fb5ae94ae7d7d5cdc4932df08cf
Ivan Delaš <ivandelas@gmail.com>
2018-09-18 12:30:48 +0200

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+  validate :click_bait?
+
+  def click_bait?
+    errors.add(:title, 'must be clickbait') unless title && (title.match(/Won't\sBelieve|Secret|Top\d|Guess/).class == MatchData)
+  end
 end
